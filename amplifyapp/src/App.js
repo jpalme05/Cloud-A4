1f134ae36154dfb754b29362ee0e969c0386db83
jpalme05 <jpalme05@uoguelph.ca>
2020-11-22 15:25:57 -0500

    Initial Commit

---
+++ amplifyapp/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a> LOL BOOMER </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
